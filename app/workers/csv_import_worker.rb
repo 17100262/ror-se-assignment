8a424ad91ed2a60fa0c64995094590d8b6b5ddb4
Ammar <ammar.khalid@isoftstudios.com>
2024-03-13 23:27:44 +0500

    Added error handling in controller and fixed minor issue

--- app/workers/csv_import_worker.rb
+++ app/workers/csv_import_worker.rb
@@ -1,21 +1,27 @@
-# app/jobs/csv_import_job.rb
+# app/jobs/csv_import_worker.rb
 class CsvImportWorker
     include Sidekiq::Worker
     queue_as :default
-
+    require 'csv'
     BATCH_SIZE = 1000
   
     def perform(user_id, file_path)
-        user = User.find(user_id)
+      user = User.find(user_id)
+  
+      begin
         # Perform the CSV import in batches using Sidekiq
         CSV.foreach(file_path, headers: true, encoding: 'utf-8').each_slice(BATCH_SIZE) do |rows|
-            # Use a transaction for better performance and reliability
-            ActiveRecord::Base.transaction do
-                rows.each do |row|
-                    user.blogs.create!(row.to_h)
-                end
+          # Use a transaction for better performance and reliability
+          ActiveRecord::Base.transaction do
+            rows.each do |row|
+              user.blogs.create!(row.to_h)
             end
+          end
         end
+      rescue StandardError => e
+        Rails.logger.error("CsvImportWorker Error: #{e.message}")
+        raise e
+      end
     end
-end
+  end
   
